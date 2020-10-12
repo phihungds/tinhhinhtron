11b013dadb7b1c69a24c3934d93afa953a31d70b
Phi Hung <phihunds1@gmail.com>
2020-10-12 17:30:15 +0700

    them phep tinh

---
+++ tinh.js
@@ -0,0 +1,7 @@
+let R = Number(prompt("Nhập bán kính"))
+let D = R * 2
+let C = 2 * R * 3.14
+let S = R ^ 2 * 3.14
+document.write("Đườngkính hình tròn là: " + D)
+document.write("Chu vi hình tròn là: " + C)
+document.write("Diện tích hình tròn là: " + S)
